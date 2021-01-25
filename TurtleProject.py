079625007899f0c2970ffd295c81a81ce3aa1b03
Karina Doynova <kdoynova@gmail.com>
2021-01-25 17:47:54 +0100

    Create TurtleProject.py

---
+++ TurtleProject.py
@@ -0,0 +1,16 @@
+import turtle 
+
+print ('Please enter the length of each side:')
+side_len = int(input())
+
+turtle.forward(side_len)
+turtle.left(90)
+turtle.forward(side_len)
+turtle.left(90)
+turtle.forward(side_len)
+turtle.left(90)
+turtle.forward(side_len)
+turtle.left(90)
+
+turtle.hideturtle()
+turtle.done()
