84597bac2fd678c38c400cb9c6a7ef9380b04ac8
Karina Doynova <kdoynova@gmail.com>
2021-01-25 17:45:00 +0100

    PythonBasics

---
+++ NumericalDataTypes.py
@@ -0,0 +1,9 @@
+print('Please, enter the numver of days you have travelled ')
+days_travelled = int(input())
+
+full_weeks = days_travelled // 7
+
+remaining_days = days_travelled % 7
+
+print (days_travelled,"days are", full_weeks, "weeks and", remaining_days, "days" )
+
